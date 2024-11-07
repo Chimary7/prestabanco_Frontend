db44f7b82ddfd02171b4f4508aa5f861fb4efcba
Chimary7 <jazmin.melo@usach.cl>
2024-11-07 12:07:58 -0300

    cambio boton

--- PrestaBanco-Frontend/src/http-common.js
+++ PrestaBanco-Frontend/src/http-common.js
@@ -4,7 +4,7 @@ const prestaBancoBackendServer = import.meta.env.VITE_PRESTABANCO_BACKEND_SERVER
 const prestaBancoBackendPort = import.meta.env.VITE_PRESTABANCO_BACKEND_PORT;
 
 export default axios.create({
-    baseURL: `http://${prestaBancoBackendServer}:${prestaBancoBackendPort}`,
+    baseURL: `http://20.189.161.172:8080`,
     headers: {
         'Content-type': 'application/json',
     }
